268cda2c9482d20cc3d50b2ebad89ddd1acee37a
carlos22-R <rc19102@ues.edu.sv>
2023-05-23 21:26:45 -0600

    utilizando fetch

--- aplicacion.js
+++ aplicacion.js
@@ -26,35 +26,52 @@ var categorias=[
     url:"comercio.html"
 },
     {
-    title:"Comida",
+    title:"Bebida",
     image:"https://img.freepik.com/vector-gratis/hamburguesa-realista-aislada_1284-12692.jpg?w=740&t=st=1684813116~exp=1684813716~hmac=91061849a0f3a7e06b62ee956c7cd46d1758371ae59973813af273f277a7dd54",
     url:"comercio.html"
 },
 ]
-// fetch('https://api.example.com/cards')
-//   .then(response => response.json())
-//   .then(data => {
-//     // Aquí puedes llamar a una función para renderizar los datos obtenidos
-//     renderCards(data);
-//   })
-//   .catch(error => {
-//     console.error('Error al obtener los datos de la API:', error);
-//   });
+let datalocal
+async function traerdatos(){
+    // var dataSql = []
+    // fetch('http://localhost:8080/ParcialTPI/comercio')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     // Aquí puedes llamar a una función para renderizar los datos obtenidos
+    //     dataSql.push(data)
+    //   })
+    //   .catch(error => {
+    //     console.error('Error al obtener los datos de la API:', error);
+    //   });
+    // //   console.log(dataSql)
+    let dataSql=fetch('http://localhost:8080/ParcialTPI/comercio')
+        .then(response => response.json())
+        .catch(error => {
+                console.error('Error al obtener los datos de la API:', error);
+              })
+    return dataSql
+}
+
 
 function renderCards(categorias) {
     const container = document.getElementById('categorias-container');
     container.innerHTML = ''; // Limpia el contenedor antes de renderizar los nuevos componentes
   
+  categorias.forEach(img=>{
+    img.image="https://img.freepik.com/vector-gratis/hamburguesa-realista-aislada_1284-12692.jpg?w=740&t=st=1684813116~exp=1684813716~hmac=91061849a0f3a7e06b62ee956c7cd46d1758371ae59973813af273f277a7dd54"
+  })
+  console.log(categorias)
     categorias.forEach(cardData => {
         const card = new Card();
         card.setAttribute('image', cardData.image);
-        card.setAttribute('title', cardData.title);
+        card.setAttribute('title', cardData.nombre);
         card.setAttribute('url', cardData.url);
         container.appendChild(card);
       });
   }
-  renderCards(categorias);
-
+datalocal= await traerdatos()
+  renderCards(datalocal);
+  
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
